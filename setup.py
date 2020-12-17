39ad3f3662f58a73e317e367b55fddafe0373bb8
cypg09 <74655692+cypg09@users.noreply.github.com>
2020-12-17 22:33:29 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='spaced_learning',
+    version='1.0.0',
+    packages=['tglog'],
+    url='https://github.com/cypg09/spaced_learning_telegram_bot',
+    license='AGPL-3.0 License',
+    author='cypg09',
+    author_email='cypg09@protonmail.com',
+    description='Spaced Learning Telegram Bot'
+)
