7f63cc34521a7028e3ce25ff8351343581cd9526
cypg09 <74655692+cypg09@users.noreply.github.com>
2020-12-23 21:18:24 +0100

    Add files via upload

--- spaced_learning/spaced_learning.py
+++ spaced_learning/spaced_learning.py
@@ -31,6 +31,7 @@ class OwnSpacedLearnHub:
         self.planning = pd.DataFrame(columns=['Description', 'J', 'date', 'id', 'check'])
         self.tlessons = None
         self.last_day = get_today()
+        self.tutorial()
         self.tl.info("New OwnSpacedLearnHub: init done.")
 
     # Telegram handling
@@ -41,25 +42,25 @@ class OwnSpacedLearnHub:
         return
 
     def tutorial(self):
-        message = "Welcome in this Telegram spaced learning bot !\n"
-        message += "It's utility is to help you learning efficiently. For this, you'll have to learn each one "
-        message += "of your lesson four times, with a specific interval between each learn session.\n"
-        message += "Everyday, at 6am, you'll receive a message summing up what you'll have to learn : "
-        message += "the title of the lessons, and their ids.\n"
-        message += "Commands:\n"
-        message += "- To register a new lesson in the system, you have to send a message with this syntax:\n"
-        message += "J0 title_of_lesson\n"
-        message += "Ex: J0 Nathan Asie chap 18\n"
-        message += "- To mark a lesson as read, you have to send a message with this syntax:\n"
-        message += "id_of_lesson done\n"
-        message += "Ex: 186 done\n"
-        message += "- If you want to delete a lesson, send a message with this syntax:\n"
-        message += "id_of_lesson del\n"
-        message += "Ex: 389 del\n"
-        message += "- To see the lessons you have to learn today, send /today.\n"
-        message += "- To see all your lessons, send /list_all.\n"
-        message += "\n"
-        message += "Good luck !"
+        message = ("Welcome in this Telegram spaced learning bot !\n"
+                   "It's utility is to help you learning efficiently. For this, you'll have to learn each one "
+                   "of your lesson four times, with a specific interval between each learn session.\n"
+                   "Everyday, at 6am, you'll receive a message summing up what you'll have to learn : "
+                   "the title of the lessons, and their ids.\n"
+                   "Commands:\n"
+                   "- To register a new lesson in the system, you have to send a message with this syntax:\n"
+                   "J0 title_of_lesson\n"
+                   "Ex: J0 Nathan Asie chap 18\n"
+                   "- To mark a lesson as read, you have to send a message with this syntax:\n"
+                   "id_of_lesson done\n"
+                   "Ex: 186 done\n"
+                   "- If you want to delete a lesson, send a message with this syntax:\n"
+                   "id_of_lesson del\n"
+                   "Ex: 389 del\n"
+                   "- To see the lessons you have to learn today, send /today.\n"
+                   "- To see all your lessons, send /list_all.\n"
+                   "\n"
+                   "Good luck !")
         self.send_telegram(message)
 
     def from_messages_dict_to_list(self, messages: dict):
@@ -85,16 +86,24 @@ class OwnSpacedLearnHub:
             self.tutorial()
             return None
         elif command == '/commands':
-            answer = "- To mark a lesson as learnt, send its id + done.\n"
-            answer += "Ex: 189 done\n"
-            answer += "- To delete a lesson from database, send one of its id + del.\n"
-            answer += "Ex: 638 del"
+            answer = ("- To mark a lesson as learnt, send its id + done.\n"
+                      "Ex: 189 done\n"
+                      "- To delete a lesson from database, send one of its id + del.\n"
+                      "Ex: 638 del")
             return answer
         elif command.startswith("J0 "):
             lesson_title = command[2:]
             self.append_new_lesson(lesson_title)
             answer = f"{lesson_title} registered !"
             return answer
+        elif command.startswith("append ") and len(command) > len("append "):
+            number_of_day = command[len('append '):]
+            if number_of_day.isdigit():
+                number_of_day = int(number_of_day)
+                self.decrease_date_to_db(number_of_day)
+                return f"Decreased {number_of_day} day."
+            else:
+                return "Syntax: append X where X is a digit."
         elif " " in list(command) and len(command.split(' ')) == 2 and command.split(' ')[0].isdigit():
             command = command.split(' ')
             _id = int(command[0])
@@ -102,6 +111,10 @@ class OwnSpacedLearnHub:
                 if command[1] == 'done':
                     self.planning.at[_id, 'check'] = True
                     answer = f"Congratulations for having finished {_id} !"
+                    return answer
+                elif command[1] == 'undone':
+                    self.planning.at[_id, 'check'] = False
+                    answer = f"Oops, undone {_id} !"
                     return answer
                 elif command[1] == 'del':
                     description = self.planning.at[_id, 'Description']
@@ -154,9 +167,9 @@ class OwnSpacedLearnHub:
         else:
             return False
 
-    def decrease_date_to_db(self):
+    def decrease_date_to_db(self, value=1):
         for index in self.planning.index.values:
-            self.planning.at[index, 'date'] -= 1
+            self.planning.at[index, 'date'] -= value
         return
 
     def today_lessons(self):
@@ -170,7 +183,7 @@ class OwnSpacedLearnHub:
         message = "Lessons of the day: \n\n"
         for index in self.tlessons.index.values:
             lesson = self.tlessons.T[index]
-            message += f"J{lesson['J']} : {lesson['description']} (id: {lesson['id']})\n"
+            message += f"J{lesson['J']} : {lesson['Description']} (id: {lesson['id']})\n"
         message += "\nSend 'id done' to mark lesson as learnt."
         return message
 
